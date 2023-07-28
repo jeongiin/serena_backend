c4bdff31e7819eb6440e5ecf70116f5217401291
Mintflavor <hamer1009@gmail.com>
2023-07-28 11:36:15 +0900

    Comment: 함수 주석 수정

--- back_api/apis/music.py
+++ back_api/apis/music.py
@@ -8,7 +8,7 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 music_api = APIRouter(prefix='/music', tags=['music'])
 
 
-# 생성 이미지 가져오기
+# 생성 음악 가져오기
 @music_api.get("/{user_id}/{music_id}")
 async def get_generated_music(user_id: UUID4, music_id: UUID4):
     return HTTPException(status_code=501, detail="Not implemented (get_generated_music)")
