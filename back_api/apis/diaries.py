43aebb28ea6b16d86092dd2d3ddfe1684c6b7273
Mintflavor <hamer1009@gmail.com>
2023-07-28 09:40:58 +0900

    Feat: response body 추가

--- back_api/apis/diaries.py
+++ back_api/apis/diaries.py
@@ -1,7 +1,20 @@
 import warnings
+from pydantic import BaseModel
+from uuid import UUID
 
 from fastapi import HTTPException, APIRouter
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
 diaries_api = APIRouter(prefix='/diaries', tags=['diaries'])
+
+
+class Diary(BaseModel):
+    user_id: UUID
+    title: str
+    content: str
+
+
+@diaries_api.post("/")
+async def create_diary():
+    pass
