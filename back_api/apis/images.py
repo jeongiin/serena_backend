5e0d678a7057e0dc5557bbf1b76f89e166bbfd72
Mintflavor <hamer1009@gmail.com>
2023-07-28 11:36:55 +0900

    Refactor: 함수 이름, URL 변경 생성 이미지 -> 생성 앨범아트 이미지

--- back_api/apis/images.py
+++ back_api/apis/images.py
@@ -8,7 +8,7 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 images_api = APIRouter(prefix='/images', tags=['images'])
 
 
-# 생성 이미지 가져오기
-@images_api.get("/{user_id}/{image_id}")
-async def get_generated_image(user_id: UUID4, image_id: UUID4):
-    return HTTPException(status_code=501, detail="Not implemented (get_generated_image)")
+# 생성 앨범아트 이미지 가져오기
+@images_api.get("/albumart/{user_id}/{image_id}")
+async def get_generated_albumart_image(user_id: UUID4, image_id: UUID4):
+    return HTTPException(status_code=501, detail="Not implemented (get_generated_albumart_image)")
