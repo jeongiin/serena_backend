772f43e0589fdfe54eafe8beefe036473e61f3ff
Mintflavor <hamer1009@gmail.com>
2023-07-28 16:20:09 +0900

    Feat: 공통 API 라우터 추가

--- back_api/main.py
+++ back_api/main.py
@@ -4,19 +4,21 @@ import uvicorn
 from fastapi import FastAPI
 
 try:
-    from .apis import diaries, letters, chats, music, images
+    from .apis import diaries, letters, chats, music, images, common
 except ImportError:
-    from apis import diaries, letters, chats, music, images
+    from apis import diaries, letters, chats, music, images, common
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
 app = FastAPI(title="SKY FLY AI Melovision Internal API Service",
               redoc_url=None)
+app.include_router(common.common_api)
 app.include_router(diaries.diaries_api)
 app.include_router(letters.letters_api)
 app.include_router(chats.chats_api)
 app.include_router(music.music_api)
 app.include_router(images.images_api)
+
 
 
 @app.get("/")
