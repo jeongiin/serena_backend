e3ecbac68a98a70b1b5b8127943ea054e880b6c3
Mintflavor <hamer1009@gmail.com>
2023-07-31 20:01:53 +0900

    Refactor: import 최적화 - __init__.py 파일 임포트 할 때 필요한 패키지 불러오게 수정

--- back_api/tests/test_main.py
+++ back_api/tests/test_main.py
@@ -1,9 +1,3 @@
-import pytest
-from httpx import AsyncClient
-
-from ..main import app
-
-
 @pytest.mark.anyio
 async def test_root():
     async with AsyncClient(app=app, base_url="http://localhost") as ac:
