f5d1f4c83f3e413907b9cc0740bfe99fc2b90b09
Mintflavor <hamer1009@gmail.com>
2023-07-31 20:03:30 +0900

    Revert "Refactor: import 최적화"
    This reverts commit e3ecbac68a98a70b1b5b8127943ea054e880b6c3.

--- back_api/tests/test_main.py
+++ back_api/tests/test_main.py
@@ -1,3 +1,9 @@
+import pytest
+from httpx import AsyncClient
+
+from ..main import app
+
+
 @pytest.mark.anyio
 async def test_root():
     async with AsyncClient(app=app, base_url="http://localhost") as ac:
