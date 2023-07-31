20c3053821df437aae3f4dd6341184aa454bce4c
Mintflavor <hamer1009@gmail.com>
2023-07-31 20:12:24 +0900

    Feat: 채팅, 편지 관련 함수 기본 로직 구현

--- back_api/apis/chats.py
+++ back_api/apis/chats.py
@@ -1,9 +1,10 @@
 import warnings
 
 from fastapi import HTTPException, APIRouter
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 
-from . import MeloDB
+from . import MeloDB, object_id_to_str, str_to_object_id
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
@@ -18,20 +19,43 @@ class Chat(BaseModel):
     content: str
 
 
-
 # 채팅 작성
 @chats_api.post("/")
 async def create_chat(item: Chat):
-    raise HTTPException(status_code=501, detail="Not implemented (create_chat)")
+    chat_id = MeloDB.melo_chats.insert_one(item.model_dump(mode='json')).inserted_id
+
+    return JSONResponse(status_code=201, content={"chat_id": str(chat_id)})
 
 
 # 전체, 개별 채팅 조회
 @chats_api.get("/")
 async def get_chats(user_id: str, baby_id: str, chat_id: str = None):
-    raise HTTPException(status_code=501, detail="Not implemented (get_chats)")
+    if chat_id:
+        chat_id = str_to_object_id(chat_id)
+        chat = MeloDB.melo_chats.find_one({"_id": chat_id, "user_id": user_id, "baby_id": baby_id})
+        if not chat:
+            raise HTTPException(status_code=404, detail="Not found")
+
+        chat['_id'] = str(chat['_id'])
+
+        return JSONResponse(status_code=200, content=chat)
+    else:
+        chats = MeloDB.melo_chats.find({"user_id": user_id, "baby_id": baby_id})
+        chats = object_id_to_str(chats)
+        if not chats:
+            raise HTTPException(status_code=404, detail="Not found")
+
+        return JSONResponse(status_code=200, content=chats)
 
 
 # 채팅 삭제
 @chats_api.delete("/")
 async def delete_chat(user_id: str, baby_id: str, chat_id: str):
-    raise HTTPException(status_code=501, detail="Not implemented (delete_chat)")
+    chat_id = str_to_object_id(chat_id)
+    chat = MeloDB.melo_chats.find_one({"_id": chat_id, "user_id": user_id, "baby_id": baby_id})
+    if not chat:
+        raise HTTPException(status_code=404, detail="Not found")
+
+    MeloDB.melo_chats.delete_one({"_id": chat_id, "user_id": user_id, "baby_id": baby_id})
+
+    return JSONResponse(status_code=200, content={"chat_id": str(chat_id)})
--- back_api/apis/letters.py
+++ back_api/apis/letters.py
@@ -1,9 +1,10 @@
 import warnings
 
 from fastapi import HTTPException, APIRouter
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 
-from . import MeloDB
+from . import MeloDB, object_id_to_str, str_to_object_id
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
@@ -18,26 +19,56 @@ class Letter(BaseModel):
     content: str
 
 
-
 # 편지 작성
 @letters_api.post("/")
 async def create_letter(item: Letter):
-    raise HTTPException(status_code=501, detail="Not implemented (create_letter)")
+    letter_id = MeloDB.melo_letters.insert_one(item.model_dump(mode='json')).inserted_id
+
+    return JSONResponse(status_code=201, content={"letter_id": str(letter_id)})
 
 
 # 전체, 개별 편지 조회
 @letters_api.get("/")
 async def get_letters(user_id: str, baby_id: str, letter_id: str = None):
-    raise HTTPException(status_code=501, detail="Not implemented (get_letters)")
+    if letter_id:
+        letter_id = str_to_object_id(letter_id)
+        letter = MeloDB.melo_letters.find_one({"_id": letter_id, "user_id": user_id, "baby_id": baby_id})
+        if not letter:
+            raise HTTPException(status_code=404, detail="Not found")
+
+        letter['_id'] = str(letter['_id'])
+
+        return JSONResponse(status_code=200, content=letter)
+    else:
+        letters = MeloDB.melo_letters.find({"user_id": user_id, "baby_id": baby_id})
+        letters = object_id_to_str(letters)
+        if not letters:
+            raise HTTPException(status_code=404, detail="Not found")
+
+        return JSONResponse(status_code=200, content=letters)
 
 
 # 편지 수정
 @letters_api.put("/")
 async def update_letter(letter_id: str, item: Letter):
-    raise HTTPException(status_code=501, detail="Not implemented (update_letter)")
+    letter_id = str_to_object_id(letter_id)
+    letter = MeloDB.melo_letters.find_one({"_id": letter_id, "user_id": item.user_id, "baby_id": item.baby_id})
+    if not letter:
+        raise HTTPException(status_code=404, detail="Not found")
+
+    MeloDB.melo_letters.update_one({"_id": letter_id, "user_id": item.user_id, "baby_id": item.baby_id}, {"$set": item.model_dump(mode='json')})
+
+    return JSONResponse(status_code=200, content={"letter_id": str(letter_id)})
 
 
 # 편지 삭제
 @letters_api.delete("/")
 async def delete_letter(user_id: str, baby_id: str, letter_id: str):
-    raise HTTPException(status_code=501, detail="Not implemented (delete_letter)")
+    letter_id = str_to_object_id(letter_id)
+    letter = MeloDB.melo_letters.find_one({"_id": letter_id, "user_id": user_id, "baby_id": baby_id})
+    if not letter:
+        raise HTTPException(status_code=404, detail="Not found")
+
+    MeloDB.melo_letters.delete_one({"_id": letter_id, "user_id": user_id, "baby_id": baby_id})
+
+    return JSONResponse(status_code=200, content={"letter_id": str(letter_id)})
