bf1274bcc84ce138381f8943b4f77c347de0be92
Mintflavor <hamer1009@gmail.com>
2023-08-02 17:28:44 +0900

    Fix: common 테스트 코드 수정 - 초기 테스트 계정, 아기 추가 - params, request body 수정

--- back_api/tests/test_common.py
+++ back_api/tests/test_common.py
@@ -1,5 +1,33 @@
 from . import *
 
+from ..apis import MeloDB
+
+# 테스트로 사용할 회원 정보 작성
+MeloDB = MeloDB()
+user = {
+    'name': '김나박이',
+    'email': 'asdasd@asd.com',
+    'phone': '010-1234-5678',
+    'address': '서울시 어딘가',
+    'description': 'Hi There'
+}
+test_user_id = MeloDB.melo_users.insert_one(user).inserted_id
+
+# 테스트로 사용할 아기 정보 작성
+baby1 = {
+    'user_id': test_user_id,
+    'name': '튼튼이',
+    'sex': 'male',
+    'birth': '2020-01-01'
+}
+baby2 = {
+    'user_id': test_user_id,
+    'name': '건강이',
+    'sex': 'female',
+    'birth': '2023-01-01'
+}
+test_baby1_id = MeloDB.melo_babies.insert_one(baby1).inserted_id
+test_baby2_id = MeloDB.melo_babies.insert_one(baby2).inserted_id
 
 # 새 회원 정보 작성 테스트
 # @pytest.mark.anyio
@@ -22,7 +50,7 @@ from . import *
 # 회원 정보 조회 테스트
 @pytest.mark.anyio
 @pytest.mark.parametrize("user_id, result",
-                         [('64c7919446157ab0a00eb2c5', 200)])
+                         [(test_user_id, 200)])
 async def test_get_user(user_id, result):
     async with AsyncClient(app=app, base_url="http://localhost") as ac:
         params = {"user_id": user_id}
@@ -34,7 +62,7 @@ async def test_get_user(user_id, result):
 # 회원 정보 수정 테스트
 @pytest.mark.anyio
 @pytest.mark.parametrize("user_id, name, email, phone, address, description, result",
-                         [('64c7919446157ab0a00eb2c5', 'test', '', '', '', '', 200)])
+                         [(test_user_id, '수정된 이름', '수정된 이메일', '수정된 번호', '수정된 주소', '수정된 설명', 200)])
 async def test_update_user(user_id, name, email, phone, address, description, result):
     async with AsyncClient(app=app, base_url="http://localhost") as ac:
         request_body = {
@@ -82,8 +110,8 @@ async def test_update_user(user_id, name, email, phone, address, description, re
 # 아기 정보 조회 테스트
 @pytest.mark.anyio
 @pytest.mark.parametrize("user_id, baby_id, result",
-                         [("64c7919446157ab0a00eb2c5", None, 200),
-                          ("64c7919446157ab0a00eb2c5", "64c792c686bd92a11fa8546b", 200)])
+                         [(test_user_id, None, 200),
+                          (test_user_id, test_baby1_id, 200)])
 async def test_get_baby(user_id, baby_id, result):
     async with AsyncClient(app=app, base_url="http://localhost") as ac:
         params = {"user_id": user_id, "baby_id": baby_id}
@@ -95,11 +123,12 @@ async def test_get_baby(user_id, baby_id, result):
 # 아기 정보 수정 테스트
 @pytest.mark.anyio
 @pytest.mark.parametrize("user_id, baby_id, name, sex, birth, result",
-                         [("64c7919446157ab0a00eb2c5", "64c792c686bd92a11fa8546b", "건강이", "female", "2021-01-01", 200)])
+                         [(test_user_id, test_baby1_id, "수정된 튼튼이", "female", "2021-01-01", 200)])
 async def test_update_baby(user_id, baby_id, name, sex, birth, result):
     async with AsyncClient(app=app, base_url="http://localhost") as ac:
-        params = {"user_id": user_id, "baby_id": baby_id}
+        params = {"baby_id": baby_id}
         request_body = {
+            "user": user_id,
             "name": name,
             "sex": sex,
             "birth": birth
