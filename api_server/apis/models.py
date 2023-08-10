8a1edf1465163899d84305b1bb65241bcd316c54
Mintflavor <hamer1009@gmail.com>
2023-08-10 11:32:31 +0900

    Feat: 음악 생성 Request Body 수정 - 컨텐츠 타입, 아이디 주석화(미사용) - music의 _id string 변환 함수 수정

--- api_server/apis/models.py
+++ api_server/apis/models.py
@@ -13,10 +13,10 @@ MeloDB = MeloDB()
 models_api = APIRouter(prefix='/models', tags=['models'])
 
 
-class ContentType(str, Enum):
-    diary = 'diary'
-    letter = 'letter'
-    chat = 'chat'
+# class ContentType(str, Enum):
+#     diary = 'diary'
+#     letter = 'letter'
+#     chat = 'chat'
 
 
 class Genre(str, Enum):
@@ -52,8 +52,8 @@ class Duration(str, Enum):
 class ImageGenerateQuery(BaseModel):
     user_id: str
     baby_id: str
-    content_type: ContentType
-    content_id: str
+    # content_type: ContentType
+    # content_id: str
     genre: str
     instrument: str
     speed: str
@@ -63,8 +63,8 @@ class ImageGenerateQuery(BaseModel):
 class MusicGenerateQuery(BaseModel):
     user_id: str
     baby_id: str
-    content_type: ContentType
-    content_id: str
+    # content_type: ContentType
+    # content_id: str
     genre: str
     instrument: str
     speed: str
@@ -74,16 +74,16 @@ class MusicGenerateQuery(BaseModel):
 # 생성 앨범아트 이미지 생성하기
 @models_api.post("/images")
 async def create_generated_albumart_image(item: ImageGenerateQuery):
-    content_type_map = {
-        ContentType.diary: MeloDB.melo_diaries,
-        ContentType.letter: MeloDB.melo_letters,
-        ContentType.chat: MeloDB.melo_chats,
-    }
-
-    content_id = str_to_object_id(item.content_id)
-    content = content_type_map[item.content_type].find_one({"_id": content_id, "user_id": item.user_id, "baby_id": item.baby_id})
-    if not content:
-        raise HTTPException(status_code=404, detail="Not found")
+    # content_type_map = {
+    #     ContentType.diary: MeloDB.melo_diaries,
+    #     ContentType.letter: MeloDB.melo_letters,
+    #     ContentType.chat: MeloDB.melo_chats,
+    # }
+    #
+    # content_id = str_to_object_id(item.content_id)
+    # content = content_type_map[item.content_type].find_one({"_id": content_id, "user_id": item.user_id, "baby_id": item.baby_id})
+    # if not content:
+    #     raise HTTPException(status_code=404, detail="Not found")
 
     # -------------------------------------------
     # # TODO: 모델에 이미지 생성 요청하는 코드 작성
@@ -125,16 +125,16 @@ async def delete_generated_albumart_image(user_id: str, baby_id: str, image_id: 
 # 생성 음악 생성하기
 @models_api.post("/music")
 async def create_generated_music(item: MusicGenerateQuery):
-    content_type_map = {
-        ContentType.diary: MeloDB.melo_diaries,
-        ContentType.letter: MeloDB.melo_letters,
-        ContentType.chat: MeloDB.melo_chats,
-    }
-
-    content_id = str_to_object_id(item.content_id)
-    content = content_type_map[item.content_type].find_one({"_id": content_id, "user_id": item.user_id, "baby_id": item.baby_id})
-    if not content:
-        raise HTTPException(status_code=404, detail="Not found")
+    # content_type_map = {
+    #     ContentType.diary: MeloDB.melo_diaries,
+    #     ContentType.letter: MeloDB.melo_letters,
+    #     ContentType.chat: MeloDB.melo_chats,
+    # }
+    #
+    # content_id = str_to_object_id(item.content_id)
+    # content = content_type_map[item.content_type].find_one({"_id": content_id, "user_id": item.user_id, "baby_id": item.baby_id})
+    # if not content:
+    #     raise HTTPException(status_code=404, detail="Not found")
 
     # -------------------------------------------
     # # TODO: 모델에 음악 생성 요청하는 코드 작성
@@ -155,9 +155,9 @@ async def get_generated_music(user_id: str, baby_id: str, music_id: str):
     if not music:
         raise HTTPException(status_code=404, detail="Not found")
 
-    music['id'] = object_id_to_str(music['_id'])
+    music['id'] = str(music['_id'])
 
-    return JSONResponse(status_code=200, content=music['content'])
+    return JSONResponse(status_code=200, content=music)
 
 
 # 생성 음악 제거
