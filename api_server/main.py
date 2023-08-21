b0a7bf0cf4aaf954b220b2c59b69bf2f052b6b9d
Mintflavor <hamer1009@gmail.com>
2023-08-21 11:49:43 +0900

    Feat: Request Body 오류 상세 리턴 기능 추가

--- api_server/main.py
+++ api_server/main.py
@@ -2,6 +2,8 @@ import warnings
 
 import uvicorn
 from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import PlainTextResponse
 
 try:
     from .apis import diaries, letters, chats, models, common
@@ -17,6 +19,13 @@ app.include_router(common.common_api)
 # app.include_router(letters.letters_api)
 # app.include_router(chats.chats_api)
 app.include_router(models.models_api)
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc: RequestValidationError):
+    print(exc.errors())
+    print(exc.body)
+    return PlainTextResponse(status_code=422, content=f"detail: {exc.errors()}\nbody: {exc.body}")
 
 
 @app.get("/")
